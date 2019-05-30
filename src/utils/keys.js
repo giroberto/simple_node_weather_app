374b8b12ca44b8ac23229239baaaccce5d9adb3e
giroberto <umbertobarros@gmail.com>
2019-05-30 20:01:47 -0300

    Added option to retrieve api keys from process.env

--- src/utils/keys.js
+++ src/utils/keys.js
@@ -1,4 +1,4 @@
-const mapboxtoken = 'ADD_YOUR_MAPBOX_KEY_HERE'
-const darkskytoken = 'ADD_YOUR_DARKSKY_KEY_HERE'
+const mapboxtoken = process.env.mapboxtoken  || 'ADD_YOUR_MAPBOX_KEY_HERE'
+const darkskytoken = process.env.darkskytoken || 'ADD_YOUR_DARKSKY_KEY_HERE'
 
 module.exports = { mapboxtoken, darkskytoken  } 
